757e1bd8525235ec32ca0baf4fb9b617664ee378
garrett.conaty@bea.com <garrett.conaty@bea.com@3184b893-5dd9-0310-85ef-949a2054e60f>
2004-05-15 19:29:51 +0000

    Initial sforce control check-in

---
+++ sforce/trunk/sforce3.0/controls/sforceControl/sforceControl/org/controlhaus/sforce/workshop/ide/InstallExtensionException.java
@@ -0,0 +1,54 @@
+/*   Copyright 2004 Salesforce.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.controlhaus.sforce.workshop.ide; 
+
+public class InstallExtensionException extends Exception
+{ 
+
+	/**
+	 * 
+	 */
+	public InstallExtensionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public InstallExtensionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public InstallExtensionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public InstallExtensionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+} 
