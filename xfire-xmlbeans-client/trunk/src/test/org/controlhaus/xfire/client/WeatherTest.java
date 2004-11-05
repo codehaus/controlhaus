c25041175ed506034fd033b8cbc7cbd191b08bb9
dan <dan@envoisolutions.com>
2004-11-05 19:36:36 +0000

    Add a client control with all the Metadata goodies.

--- xfire-xmlbeans-client/trunk/src/test/org/controlhaus/xfire/client/WeatherTest.java
+++ xfire-xmlbeans-client/trunk/src/test/org/controlhaus/xfire/client/WeatherTest.java
@@ -15,35 +15,9 @@ import junit.framework.TestCase;
  * @since Nov 2, 2004
  */
 public class WeatherTest 
-    extends TestCase
+    extends AbstractControlTest
 {
     @Control WeatherForecastClientControl weather;
-    
-    private ControlContainerContext context;
-    
-    public void setUp() throws Exception
-    {
-        context = new ControlContainerContext();
-        
-        try
-        {
-            Class init = getClass().getClassLoader().loadClass( 
-                    getClass().getName() + "ClientInitializer" );
-            
-            Method m = init.getMethod("initialize", 
-                                      new Class[] 
-                                      { 
-                                            ControlBeanContext.class, 
-                                            getClass() 
-                                      } );
-            
-            m.invoke( null, new Object[] { context, this } );
-        }
-        catch ( ClassNotFoundException cnfe )
-        {
-            // do nothing.
-        }
-    }
 
     public void testControl() 
         throws Exception
