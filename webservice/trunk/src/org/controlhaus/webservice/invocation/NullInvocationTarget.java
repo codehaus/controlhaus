daf1dc9584e90be484b2efedd7f914e6893a024a
jcolwell <jcolwell@bea.com>
2004-12-08 23:15:57 +0000

    Added some test cases that point at real, live web services such as the Google Web API and XMethods Query Service.
    Most of the changed file just had the copyright changed back to BEA.
    
    The key in google.key was issued to beehive@superflaco.com to provide access to
    the Google Web API. Users should get their own key to prevent this key from using
    up all 1000 hits per day.

--- webservice/trunk/src/org/controlhaus/webservice/invocation/NullInvocationTarget.java
+++ webservice/trunk/src/org/controlhaus/webservice/invocation/NullInvocationTarget.java
@@ -1,8 +1,7 @@
 /*
  * NullInvocationTarget.java
  * 
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
+ * Copyright 2004 BEA Systems, Inc.  
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
