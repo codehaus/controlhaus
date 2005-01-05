ca5944005e55f1dbb88c01c42d90ca7f14862977
cschoett <cschoett@bea.com>
2005-01-05 23:25:25 +0000

    cleanup of the env files

--- jdbc/trunk/beelineEnv.cmd
+++ jdbc/trunk/beelineEnv.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 
 REM
-REM Set the Beeline shell environment
+REM Set the JdbcControl shell environment
 REM
 REM
-REM In order to build, define BEELINE_HOME and JAVA_HOME
-REM in your shell.  Then, run the Beeline environment script.
+REM In order to build, define JDBCCONTROL_HOME and JAVA_HOME
+REM in your shell.  Then, run the this script.
 REM
 
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
