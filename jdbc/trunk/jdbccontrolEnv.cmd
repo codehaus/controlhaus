4d09a2b0951dc033b87670b856e9002ff9c1fbf5
cschoett@bea.com <cschoett@bea.com@3184b893-5dd9-0310-85ef-949a2054e60f>
2005-01-11 22:48:08 +0000

    Minor text update.

--- jdbc/trunk/jdbccontrolEnv.cmd
+++ jdbc/trunk/jdbccontrolEnv.cmd
@@ -4,7 +4,7 @@ REM
 REM Set the JdbcControl shell environment
 REM
 REM
-REM In order to build, define JDBCCONTROL_HOME, BEELINE_HOME, and JAVA_HOME
+REM In order to build, define JDBCCONTROL_HOME, BEEHIVE_HOME, and JAVA_HOME
 REM in your shell.  Then, run the this script.
 REM
 
