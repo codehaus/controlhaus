a8ae096cf61db84d25270a1a17bf678043bbbcfa
cschoett <cschoett@bea.com>
2005-03-21 17:23:41 +0000

    Refactored RowMapperFactory, mostly alot of cleanup and added some new public API's.  Changed the behavior for typical class matching to checking for equals instead of isAssignable from (with the exception of the base object mapper and XmlObject mappers)

--- jdbc/trunk/src/org/controlhaus/jdbc/RowMapperFactory.java
+++ jdbc/trunk/src/org/controlhaus/jdbc/RowMapperFactory.java
@@ -22,13 +22,13 @@ import org.apache.beehive.controls.api.ControlException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Factory for creating row mappers.
+ * <p/>
  * Row mapper types supported by this factory include: HashMap, Map, Object, XmlObject. The factory determines the
  * proper row mapper to use by checking its List of RowMappers against the type of mapping requested.  When performing
  * the lookup, the factory attempts to find the most specific type match.  If a match can't be found the most general
@@ -36,21 +36,22 @@ import java.util.Map;
  */
 public final class RowMapperFactory {
 
-    //@todo: Re-evaluate: should the _rowMappings table be a static ?
-    //@todo: currently applies to all dbcontrol instances -- also need to enhance API
-    //@todo: to allow for more control when adding an new mapper.
-    private static final ArrayList<RowMapping> _rowMappings = new ArrayList<RowMapping>();
+    private static final HashMap<Class, Class<? extends RowMapper>> _rowMappings = new HashMap<Class, Class<? extends RowMapper>>();
 
-    private static RowMapping DEFAULT_ROWMAPPING = new RowMapping(Object.class, RowToObjectMapper.class);
+    private static Class<? extends RowMapper> DEFAULT_OBJ_ROWMAPPING = RowToObjectMapper.class;
+
+    private static Class XMLOBJ_CLASS = null;
+    private static Class<? extends RowMapper> DEFAULT_XMLOBJ_ROWMAPPING = null;
+    private final static Class[] _params = {ResultSet.class, Class.class, Calendar.class};
 
     static {
 
-        _rowMappings.add(new RowMapping(HashMap.class, RowToHashMapMapper.class));
-        _rowMappings.add(new RowMapping(Map.class, RowToMapMapper.class));
+        _rowMappings.put(HashMap.class, RowToHashMapMapper.class);
+        _rowMappings.put(Map.class, RowToMapMapper.class);
 
         try {
-            Class xmlObj = Class.forName("org.apache.xmlbeans.XmlObject");
-            _rowMappings.add(new RowMapping(xmlObj, RowToXmlObjectMapper.class));
+            XMLOBJ_CLASS = Class.forName("org.apache.xmlbeans.XmlObject");
+            DEFAULT_XMLOBJ_ROWMAPPING = RowToXmlObjectMapper.class;
         } catch (ClassNotFoundException e) {
             // NOOP if apache xml beans not present
         }
@@ -67,108 +68,103 @@ public final class RowMapperFactory {
      */
     public static RowMapper getRowMapper(ResultSet rs, Class returnTypeClass, Calendar cal) throws ControlException {
 
-        for (RowMapping rm : _rowMappings) {
-            if (rm.canMapToReturnType(returnTypeClass)) {
-                return rm.getMapper(rs, returnTypeClass, cal);
-            }
+        Class<? extends RowMapper> rm = _rowMappings.get(returnTypeClass);
+        if (rm != null) {
+            return getMapper(rm, rs, returnTypeClass, cal);
         }
 
         //
-        // if we made it to here, use the default mapper
+        // if we made it to here, check if the default XMLObject Mapper can be used,
+        // otherwise use the default object mapper
         //
-        return DEFAULT_ROWMAPPING.getMapper(rs, returnTypeClass, cal);
+        if (XMLOBJ_CLASS != null && XMLOBJ_CLASS.isAssignableFrom(returnTypeClass)) {
+            return getMapper(DEFAULT_XMLOBJ_ROWMAPPING, rs, returnTypeClass, cal);
+        } else {
+            return getMapper(DEFAULT_OBJ_ROWMAPPING, rs, returnTypeClass, cal);
+        }
     }
 
     /**
-     * Append a new row mapper to the list of available row mappers.  The getRowMapper method traverses the
+     * Add a new row mapper to the list of available row mappers.  The getRowMapper method traverses the
      * list of mappers from beginning to end, checking to see if a mapper can handle the specified
      * returnTypeClass.  There is a default mapper which is used if a match cannot be found in the list.
      *
      * @param returnTypeClass Class which this mapper maps a row to.
-     * @param rowMapperClass The row mapper class.
+     * @param rowMapperClass  The row mapper class.
      */
-    public static void appendRowMapping(Class returnTypeClass, Class<? extends RowMapper> rowMapperClass) {
-        _rowMappings.add(new RowMapping(returnTypeClass, rowMapperClass));
+    public static void addRowMapping(Class returnTypeClass, Class<? extends RowMapper> rowMapperClass) {
+        _rowMappings.put(returnTypeClass, rowMapperClass);
     }
 
     /**
      * Replace a row mapping.
      *
      * @param returnTypeClass Class which this mapper maps a row to.
-     * @param rowMapperClass The row mapper class.
+     * @param rowMapperClass  The row mapper class.
      * @return if the mapper was replaced, false mapper for returnTypeClass was not found, no action taken.
      */
-    public static boolean replaceRowMapping(Class returnTypeClass, Class<? extends RowMapper> rowMapperClass) {
-        for (RowMapping rm : _rowMappings) {
-            if (rm._mapperFor == returnTypeClass) {
-                _rowMappings.set(_rowMappings.indexOf(rm), new RowMapping(rowMapperClass, rowMapperClass));
-                return true;
-            }
-        }
-        return false;
+    public static Class<? extends RowMapper> replaceRowMapping(Class returnTypeClass, Class<? extends RowMapper> rowMapperClass) {
+        return _rowMappings.put(returnTypeClass, rowMapperClass);
+    }
+
+    /**
+     * remove the  row mapping for the specified class type.
+     *
+     * @param returnTypeClass
+     * @return the RowMapper class which was removed, null if returnTypeClass did not match any of the registered
+     *         row mappers.
+     */
+    public static Class<? extends RowMapper> removeRowMapping(Class returnTypeClass) {
+        return _rowMappings.remove(returnTypeClass);
     }
 
     /**
      * Sets the rowmapper for Object.class
+     *
      * @param rowMapperClass
      */
-    public static void setDefaultRowMapping(Class<? extends RowMapper> rowMapperClass) {
-        DEFAULT_ROWMAPPING = new RowMapping(Object.class, rowMapperClass);
+    public static Class <? extends RowMapper> setDefaultRowMapping(Class<? extends RowMapper> rowMapperClass) {
+        Class<? extends RowMapper> ret = DEFAULT_OBJ_ROWMAPPING;
+        DEFAULT_OBJ_ROWMAPPING = rowMapperClass;
+        return ret;
     }
 
     /**
-     * Helper class for storing row mappers.
+     * Sets the rowmapper for XmlObject.class
+     *
+     * @param rowMapperClass
      */
-    private static final class RowMapping {
+    public static Class<? extends RowMapper> setDefaultXmlRowMapping(Class mapToClass, Class<? extends RowMapper> rowMapperClass) {
+        Class<? extends RowMapper> ret = DEFAULT_XMLOBJ_ROWMAPPING;
+        DEFAULT_XMLOBJ_ROWMAPPING = rowMapperClass;
+        XMLOBJ_CLASS = mapToClass;
+        return ret;
+    }
 
-        private final static Class[] _params = {ResultSet.class, Class.class, Calendar.class};
-        private final Class<?> _mapperFor;
-        private final Class _rowMapper;
-
-        /**
-         * Create a new RowMapping associating a specific class to a RowMapper.
-         *
-         * @param mapperFor Class this mapper maps to.
-         * @param rowMapper A RowMapper implementation.
-         */
-        RowMapping(Class mapperFor, Class<? extends RowMapper> rowMapper) {
-            _mapperFor = mapperFor;
-            _rowMapper = rowMapper;
-        }
-
-        /**
-         * Can this mapper map to the specified return type?
-         *
-         * @param returnTypeClass Class to attempt mapping to.
-         * @return true if this mapper can map the the returnTypeClass.
-         */
-        boolean canMapToReturnType(Class returnTypeClass) {
-            return _mapperFor.isAssignableFrom(returnTypeClass);
-        }
-
-        /**
-         * Create an instance of the RowMapper class.
-         *
-         * @param rs         ResultSet we are mapping from.
-         * @param returnType Class to map rows to.
-         * @param cal        Calendar instance for date/time values.
-         * @return A RowMapper instance.
-         * @throws ControlException on error.
-         */
-        RowMapper getMapper(ResultSet rs, Class returnType, Calendar cal) throws ControlException {
-            Constructor c = null;
-            try {
-                c = _rowMapper.getDeclaredConstructor(_params);
-                return (RowMapper) c.newInstance(new Object[]{rs, returnType, cal});
-            } catch (NoSuchMethodException e) {
-                throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
-            } catch (InstantiationException e) {
-                throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
-            } catch (IllegalAccessException e) {
-                throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
-            } catch (InvocationTargetException e) {
-                throw new ControlException("Failure creating new instance of RowMapper, " + e.getCause().toString());
-            }
+    /**
+     * Create an instance of the RowMapper class.
+     *
+     * @param rowMapper
+     * @param rs         ResultSet we are mapping from.
+     * @param returnType Class to map rows to.
+     * @param cal        Calendar instance for date/time values.
+     * @return A RowMapper instance.
+     * @throws ControlException on error.
+     */
+    private static RowMapper getMapper(Class<? extends RowMapper> rowMapper, ResultSet rs, Class returnType, Calendar cal)
+            throws ControlException {
+        Constructor c = null;
+        try {
+            c = rowMapper.getDeclaredConstructor(_params);
+            return (RowMapper) c.newInstance(new Object[]{rs, returnType, cal});
+        } catch (NoSuchMethodException e) {
+            throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
+        } catch (InstantiationException e) {
+            throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
+        } catch (IllegalAccessException e) {
+            throw new ControlException("Failure creating new instance of RowMapper, " + e.toString());
+        } catch (InvocationTargetException e) {
+            throw new ControlException("Failure creating new instance of RowMapper, " + e.getCause().toString());
         }
     }
 }
